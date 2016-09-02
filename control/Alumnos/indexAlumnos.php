97f39cccc8d9636b33ec254a48e70debda6e35c3
Giuliano Adriel Piazzese <gapiazzese@hotmail.com>
2016-09-02 09:55:21 -0300

    Home alumno y circle progess

--- control/Alumnos/indexAlumnos.php
+++ control/Alumnos/indexAlumnos.php
@@ -4,5 +4,6 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/primopiano/includes/functions.php';
 $twigParams = array();
 secure('indexAlumnos');
 
+$twigParams["cantSemanal"]= $_SESSION["cantSemanal"];
 $twigParams["title"]= 'Bienvenido Alumno';
 init('welcome.twig', $twigParams);
